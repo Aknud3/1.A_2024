f0a5362df3d1018dd1d97641ac61b4303a14dc81
Aknud3 <168775964+Aknud3@users.noreply.github.com>
2024-09-11 18:53:55 +0200

    Update class_of_26.py

--- class_of_26.py
+++ class_of_26.py
@@ -1,19 +1,31 @@
+"""Importing os for cleaning terminal"""
+
 import os
 
-clear = lambda: os.system("cls")
+
+def clear():
+    """clearing terminal"""
+    os.system("cls")  # windows
+    os.system("clear")  # linux
 
 
 class Game:
+    """class game, that runs everything"""
+
     def __init__(self):
         self.self = self
 
     def pick_a_choice(self, a, b, c):
+        """pick a choice in game"""
         print(
             "\n\033[35mVyberte si mezi těmito možnostmi: "
             f"\033[0m\n \033[31m{a}\033[0m \033[32m{b}\033[0m \033[34m{c}\033[0m"
         )
+        answer = input("Napište a, b nebo c: \n")
+        return answer
 
     def next_dialogue(self):
+        """Next dialogue in game, only ends when we ENTER"""
         print("\n\033[34mPro pokrok, zmáčkněte\033[0m \033[32m[ENTER]\033[0m")
         while True:
             user_input = input()
@@ -22,8 +34,9 @@ class Game:
         clear()
 
     def starting_credits(self):
+        """The main logo of game"""
         print(
-            """      
+            r"""      
   __              ___   ___ ___  _  _   
  /_ |     /\     |__ \ / _ \__ \| || |  
   | |    /  \       ) | | | | ) | || |_ 
@@ -42,34 +55,77 @@ class Game:
         self.next_dialogue()
 
     def start_of_game(self):
-        print(
-            """Je to vtipné, když holka říká svůj příběh...
-A jen jí řeknou, že je to nereálné
-Téměř jako by se báli uvěřit, že je to SKUTEČNÉ.
-Pro všechny, kdo to nevědí...
-Moje máma je SVINĚ, můj táta se zastřelil, můj život je na houby,
-A můj brácha je na SEZNAMU sledovaných.
-Ne nutně v tomhle pořadí... nebo možná, já nevím.
-Ale minulý rok...
-Mě tahle kombinace dostala na tu NEJHORŠÍ, nejnebezpečnější školu, co si jde představit.
-Aspoň jsem si to myslela.
-Pak jsem mluvila s ostatními a ukázalo se, že to nebylo tak hrozné?
-No, ne, BYLO to tak hrozné, jen to bylo docela standardní.
-Jiné holky říkaly, že jejich tělocvikář s nimi chtěl spát, 
-jejich poradce jim psal na Teamsy ve 3 ráno.
-Já byla jediná, kdo měl za učitele fotografa, který byl bílý nácek. Takže to bylo aspoň něco...
-ALE TADY JE PROBLÉM:
-Máma neposlouchá,
-ředitelce je to JEDNO, a já mám ještě CELÝ poslední ročník před sebou.
-Můj život je jen hra, nemocná a beznadějná hra.
-Nikdy jsem nebyla nějak zvlášť „věřící“, 
-ale jestli je Bůh nějaká introvertní nula sedící u svého počítače...
-Opravdu doufám, že mi může pomoci tohle zvládnout."""
-        )
+        """Dialogue on the start of the game"""
+        print(""" """)
         self.next_dialogue()
 
+    def first_choice(self):
+        """Make the first choice of the game"""
+        print(""" """)
+        answer = self.pick_a_choice("a", "b", "c")
+        self.next_dialogue()
+        return answer
+    # https://www.reddit.com/r/Classof09Game/comments/17uta4i/class_of_09_reup_game_endings_guide/
+    # this is the inspiration for this, and the tree works from left is A and right is C middle option is B
+    # Main 3 routes
+    def route_a(self):
+        print("a")
+
+    def route_b(self):
+        print("b")
+
+    def route_c(self):
+        print("c")
+
+    # Sub routes of 3 main routes
+    def route_a_a(self):
+        pass
+
+    def route_a_b(self):
+        pass
+
+    def route_b_a(self):
+        pass
+
+    def route_b_b(self):
+        pass
+
+    def route_c_a(self):
+        pass
+
+    def route_c_b(self):
+        pass
+
+    # Sub routes of sub routes
+    def route_a_a_a(self):
+        pass
+
+    def route_a_a_b(self):
+        pass
+
+    def route_c_a_a(self):
+        pass
+
+    def route_c_a_b(self):
+        pass
+
+    def route_c_b_a(self):
+        pass
+
+    def route_c_b_b(self):
+        pass
+
+
+# There is the start of the program
 
 clear()
 game = Game()
 game.starting_credits()
 game.start_of_game()
+answer1 = game.first_choice()
+
+for option in ("a","b","c"):
+    if answer1 == option:
+        method_name = f"route_{option}"
+        method = getattr(game, method_name, None)
+        method()
